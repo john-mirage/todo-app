7c8a4d7603e010ef6bedf03b0505c5045eb05394
john-mirage <chaosdynamix@posteo.net>
2022-04-06 16:31:52 +0200

    feat: add selectors

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Page from '@components/page'
-import '@assets/styles/index.css'
+import '@assets/styles/index.css';
+import Page from '@components/page';
+import reducer from "@reducers/root-reducer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Page />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <Page />
+        </Provider>,
+    </React.StrictMode>,
+    document.getElementById('root')
 )
