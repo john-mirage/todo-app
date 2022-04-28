b3872eec86b112a1b9e39b4c6a5a849ebb7ea623
john-mirage <chaosdynamix@posteo.net>
2022-04-28 09:31:58 +0200

    fix: fix some styles for desktop

--- src/components/todo-menu.tsx
+++ src/components/todo-menu.tsx
@@ -6,17 +6,17 @@ function TodoMenu({ completedCount, activeCount, onClearCompleted }) {
   const itemWord = activeCount === 1 ? 'item' : 'items';
 
   return (
-    <div className={classes.TodoMenu}>
-      <div className={classes.TodoMenu__section}>
-        <p className={classes.TodoMenu__remaining}>{activeCount || 'No'} {itemWord} left</p>
+    <div className={classes.todoMenu}>
+      <div className={classes.todoMenu__section}>
+        <p className={classes.todoMenu__remaining}>{activeCount || 'No'} {itemWord} left</p>
       </div>
-      <div className={classNames(classes.TodoMenu__section, classes.TodoMenu__section_middle)}>
+      <div className={classNames(classes.todoMenu__section, classes.todoMenu__section_middle)}>
         <TodoFilters/>
       </div>
-      <div className={classes.TodoMenu__section}>
+      <div className={classes.todoMenu__section}>
         {
           !!completedCount &&
-          <button onClick={onClearCompleted} className={classes.TodoMenu__clear}>Clear Completed</button>
+          <button onClick={onClearCompleted} className={classes.todoMenu__clear}>Clear Completed</button>
         }
       </div>
     </div>
