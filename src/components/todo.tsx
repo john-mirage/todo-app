90d1f413714ad069a8c8d923f2b1da59ca2bf4c1
john-mirage <chaosdynamix@posteo.net>
2022-04-28 07:28:35 +0200

    feat: add mobile styles

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -13,17 +13,23 @@ function Todo({id, todo, completeTodo, deleteTodo}) {
 
   return (
     <li className={classes.container}>
-      <CheckInput
-        id={id}
-        checked={todo.completed}
-        onChange={handleTodoCompletion}
-      />
-      <p className={classes.content}>{todo.text}</p>
-      <button className={classes.button} onClick={handleTodoDeletion}>
-        <svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
-          <path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/>
-        </svg>
-      </button>
+      <div className={classes.fixedSection}>
+        <CheckInput
+          id={id}
+          checked={todo.completed}
+          onChange={handleTodoCompletion}
+        />
+      </div>
+      <div className={classes.fluidSection}>
+        <p className={classes.content}>{todo.text}</p>
+      </div>
+      <div className={classes.fixedSection}>
+        <button className={classes.button} onClick={handleTodoDeletion}>
+          <svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+            <path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/>
+          </svg>
+        </button>
+      </div>
     </li>
   )
 }
